265b2a5e08123be7946c231a3f40539f7dfe38fa
KyeYeon Cho <jgy2157@nate.com>
2017-02-22 10:57:42 +0000

    merge

--- routes/hdregist.js
+++ routes/hdregist.js
@@ -1,15 +1,33 @@
-var express = require('express');
-var router = express.Router();
 
-// /hdregist 이라는 도메인으로 들어오면 아래 메소드를 실행시키라느 의미
-//app.js에서 app.use('/hdregist', hdregist); 를통해 hdregist.js를 호출했고, 여기로 연결됨
-router.get('/list', function(req, res, next) {
-    //views폴더안에있는 hdregist.ejs를 띄우고, title이라느 이름으로 testing 데이터를 전달하라
-    res.render('hdregist', {
-        testing: '값 전달 테스트'
+module.exports = function(app, connectionPool) {
+
+    app.get('/hdregist', function(req, res, next) {
+        
+        /* session 없을 땐 로그인 화면으로 */
+        if(!req.session.user_name) {
+            res.redirect('/');
+        }
+    
+        console.log("session : " + req.session.user_name+" / "+req.session.emp_num);
+        
+        connectionPool.getConnection(function(err, connection) {
+            connection.query('select * from user where 1=1 and user_name = ? and emp_num = ?;', [req.session.user_name, req.session.emp_num], function(error, rows) {
+                
+                console.log("rows : " + rows.length);
+                
+                if(error) {
+                    connection.release();
+                    throw error;
+                }else {
+                    if(rows.length > 0) {
+                        res.render('hdregist', {data : rows[0], session : req.session});
+                        connection.release();
+                    }else {
+                        res.redirect('/');
+                        connection.release();
+                    }    
+                }
+            });
+        });
     });
-});
-
-
-
-module.exports = router;
+}
