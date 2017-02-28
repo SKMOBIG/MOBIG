d0f28c892bbac399166255b63db9cec0273bc34f
Lewis Hyeonwoo Lim <samo57980@gmail.com>
2017-02-28 09:27:05 +0000

    dd

--- routes/hdmain.js
+++ routes/hdmain.js
@@ -14,9 +14,7 @@ module.exports = function(app, connectionPool) {
         console.log("session : " + req.session.user_name+" / "+req.session.emp_num);
         
         connectionPool.getConnection(function(err, connection) {
-            connection.query('select hm.happyday_id, hm.happyday_name, hm.place_name, hm.reg_user_id, DATE_FORMAT(hm.reg_dtm, "%Y-%m-%d") AS reg_dtm, DATE_FORMAT(hm.happyday_dt, "%m월 %d일") AS happyday_date,SUBSTR( _UTF8"일월화수목금토", DAYOFWEEK(hm.happyday_dt), 1) AS week,date_format(hm.happyday_dt,  "%H:%i") as happy_time, hm.req_point, hm.img_url, u.user_name, u.user_img  from happyday_master hm, user u where hm.reg_user_id = u.id;',  function(error, rows) {
-                
-                
+            connection.query('select hm.happyday_id, hm.happyday_name, hm.place_name, hm.reg_user_id, DATE_FORMAT(hm.reg_dtm, "%Y-%m-%d") AS reg_dtm, DATE_FORMAT(hm.happyday_dt, "%m월 %d일") AS happyday_date,SUBSTR( _UTF8"일월화수목금토", DAYOFWEEK(hm.happyday_dt), 1) AS week,date_format(hm.happyday_dt,  "%H:%i") as happy_time, hm.req_point, hm.img_url, u.user_name, u.user_img,hm.num_participants , hm.num_participants-p.curcnt as vacancy from happyday_master hm, user u, ( select hm.happyday_id, count(*) as curcnt from happyday_user_hst hp, happyday_master hm where hm.happyday_id=hp.happyday_id group by hm.happyday_id) p where hm.reg_user_id = u.id and hm.happyday_id = p.happyday_id;',  function(error, rows) {
                 if(error) {
                     connection.release();
                     throw error;
