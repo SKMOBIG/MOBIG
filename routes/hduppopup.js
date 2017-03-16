e01ea2c5e936175f7a0529c4b376a11f688afa88
KyeYeon Cho <jgy2157@nate.com>
2017-03-16 11:03:41 +0000

    update

--- routes/hduppopup.js
+++ routes/hduppopup.js
@@ -1,13 +1,18 @@
 
 module.exports = function(app, connectionPool) {
-
-    app.get('/hduppopup', function(req, res, next) {
+    
+    app.get('/detail/:id/hduppopup', function(req, res, next) {
+        
+        /* session 없을 땐 로그인 화면으로*/
+        if(!req.session.user_name) {
+            res.redirect('/');
+        }
         
         connectionPool.getConnection(function(err, connection) {
-            connection.query('select * from user where 1=1 and user_name = ? and emp_num = ?;', [req.session.user_name, req.session.emp_num], function(error, rows) {
-                
-                // console.log("rows : " + rows.length);
-                
+            //connection.query('select * from user where 1=1 and user_name = ? and emp_num = ?;', [req.session.user_name, req.session.emp_num], function(error, rows) {
+            //등록자인 경우만 
+            connection.query('select * from happyday_master a, user b where a.reg_user_id = b.id and a.happyday_id = ?;', req.params.id, function(error, rows) {    
+               
                 if(error) {
                     connection.release();
                     throw error;
@@ -24,26 +29,8 @@ module.exports = function(app, connectionPool) {
         });
     });
     
-    // app.post('/happyday_regist', function(req, res, next) {
-    //     connectionPool.getConnection(function(err, connection) {
-            
-    //         // console.log(req.body.dday_dt);
-    //         connection.query('insert into happyday_master (happyday_name, happyday_contents, reg_user_id, category_code, reg_dtm, dday_dt, happyday_dt, req_point, state, num_participants, place_name, img_url,point_rsn, update_dtm) values(?,?,?,?,date_format(sysdate(), "%Y%m%d%H%i%s"),?,?,?,"Y",?,?,?,?,date_format(sysdate(), "%Y%m%d%H%i%s"));'
-    //                             , [req.body.happyday_name, req.body.happyday_contents, req.session.user_id, req.body.category_code,req.body.dday_dt,req.body.starthappyday, req.body.req_point,req.body.num_participants, req.body.place_name, req.body.img_url, req.body.point_rsn], function(error, rows) {
-    //             if(error) {
-    //                 connection.release();
-    //                 throw error;
-    //             }else {
-    //                 connection.release();
-                   
-    //                 res.redirect('/hdregist');
-    //             }    
-                
-    //         });
-    //     });
-    // });
-    
-   app.post('/Happdaygetdata_KJY', function(req, res, next) {
+
+    /*app.post('/Happdaygetdata_KJY', function(req, res, next) {
         
         // console.log(req.body);
         
@@ -60,20 +47,20 @@ module.exports = function(app, connectionPool) {
                 
             });
         });
-    });
+    });*/
    
    
     app.post('/happyday_update', function(req, res, next) {
        
         connectionPool.getConnection(function(err, connection) {
-            connection.query('update happyday_master set happyday_name = ?, happyday_contents = ?, category_code=?, dday_dt = ?, happyday_dt = ?, req_point =?, num_participants =?, place_name = ?, img_url =?, point_rsn = ?, update_dtm = date_format(sysdate(), "%Y%m%d%H%i%s") where happyday_id = 205;'                       
-            , [req.body.happyday_name, req.body.happyday_contents, req.body.category_code, req.body.dday_dt, req.body.starthappyday, req.body.req_point, req.body.num_participants, req.body.place_name,  req.body.img_url, req.body.point_rsn], function(error, rows) {
+            connection.query('update happyday_master set happyday_name = ?, happyday_contents = ?, category_code=?, dday_dt = ?, happyday_dt = ?, req_point =?, num_participants =?, place_name = ?, img_url =?, point_rsn = ?, update_dtm = date_format(sysdate(), "%Y%m%d%H%i%s") where happyday_id = ?;'                       
+            , [req.body.happyday_name, req.body.happyday_contents, req.body.category_code, req.body.dday_dt, req.body.starthappyday, req.body.req_point, req.body.num_participants, req.body.place_name,  req.body.img_url, req.body.point_rsn, req.body.happyID], function(error, rows) {
                 if(error) {
                     connection.release();
                     throw error;
                 }else {
+                    res.redirect('/detail/'+ req.body.happyID+'/hduppopup');//add
                     connection.release();
-                   
                     res.redirect('/closepopup');
                 }    
                 
