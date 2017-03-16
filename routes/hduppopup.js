40d8880044d4e95985e6367f077029c18bfe1afd
KyeYeon Cho <jgy2157@nate.com>
2017-03-16 09:39:52 +0000

    0639

--- routes/hduppopup.js
+++ routes/hduppopup.js
@@ -66,7 +66,7 @@ module.exports = function(app, connectionPool) {
     app.post('/happyday_update', function(req, res, next) {
        
         connectionPool.getConnection(function(err, connection) {
-            connection.query('update happyday_master set happyday_name = ?, happyday_contents = ?, category_code=?, dday_dt = ?, happyday_dt = ?, req_point =?, num_participants =?, place_name = ?, img_url =?, point_rsn = ?, update_dtm = date_format(sysdate(), "%Y%m%d%H%i%s") where happyday_id = 147;'                       
+            connection.query('update happyday_master set happyday_name = ?, happyday_contents = ?, category_code=?, dday_dt = ?, happyday_dt = ?, req_point =?, num_participants =?, place_name = ?, img_url =?, point_rsn = ?, update_dtm = date_format(sysdate(), "%Y%m%d%H%i%s") where happyday_id = 205;'                       
             , [req.body.happyday_name, req.body.happyday_contents, req.body.category_code, req.body.dday_dt, req.body.starthappyday, req.body.req_point, req.body.num_participants, req.body.place_name,  req.body.img_url, req.body.point_rsn], function(error, rows) {
                 if(error) {
                     connection.release();
