ffea8c81ef48c7c88eedbd6aaa601054e3323969
dadembii <dadembii@gmail.com>
2017-03-21 11:25:09 +0000

    delete hd

--- routes/detail.js
+++ routes/detail.js
@@ -288,25 +288,11 @@ module.exports = function(app, connectionPool) {
         });
     });
 
-    
-    // app.post('/applyhappyday', function(req, res, next){
-    //     console.log("들어온다");
-    //     connectionPool.getConnection(function(err, connection) {
-    //         connection.query('insert into happyday_user_hst (user_id, happyday_id, reg_dtm, modify_dtm, use_point, state) values(?, ?, date_format(sysdate(), "%Y%m%d"), null, ?, "y");', [req.session.user_id, req.body.happyday_id, req.body.req_point], function(error, rows){
+    app.get('/deletehappyday/:happyday_id', function(req, res, next){
         
-    //             console.log("req.session.user_id : " + req.session.user_id);
-    //             if(error) {
-    //                 connection.release();
-    //                 throw error;
-    //             }else {
-    //                 res.redirect('/detail/'+ req.body.happyday_id);
-    //                 connection.release();
-    //             }
-    //         });
-    //     });
-    // });
-    
-
-
-    
+        console.log("delete : " + req.params.happyday_id);
+        
+        res.redirect('/hdmain');
+        
+    });
 }
