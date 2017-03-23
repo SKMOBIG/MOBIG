2959116cbd36189e13f3d7f97afd2cf866cdbd88
KyeYeon Cho <jgy2157@nate.com>
2017-03-23 00:15:23 +0000

    num_participants 수정

--- routes/hduppopup.js
+++ routes/hduppopup.js
@@ -1,4 +1,3 @@
-
 module.exports = function(app, connectionPool) {
     
     app.get('/detail/:id/hduppopup', function(req, res, next) {
@@ -9,12 +8,11 @@ module.exports = function(app, connectionPool) {
         }
         
         connectionPool.getConnection(function(err, connection) {
-            //connection.query('select * from user where 1=1 and user_name = ? and emp_num = ?;', [req.session.user_name, req.session.emp_num], function(error, rows) {
-             console.log("aa");
-            // connection.query('select *, concat(left(a.happyday_dt,4),"/",substring(a.happyday_dt,5,2),"/",substring(a.happyday_dt,7,2))as HDstartdtShow,  substring(a.happyday_dt,1,8) as HDstartdtDB, concat(substring(a.happyday_dt,9,2),"시",substring(a.happyday_dt,11,2),"분") as HDstarttimeShow, substring(a.happyday_dt,9,4) as HDstarttimeDB,  concat(left(a.dday_dt,4),"/",substring(a.dday_dt,5,2),"/",substring(a.dday_dt,7,2))as HDddaydtShow,  a.dday_dt as HDddaydtDB, concat(a.category_code,',',a.category_code2,',',a.category_code3) as totalCategory from happyday_master a, user b where a.reg_user_id = b.id and a.happyday_id = ?;', req.params.id, function(error, rows) {
+            //  console.log("aa");
+            connection.query('select a.*, b.*, concat(left(a.happyday_dt,4),"/",substring(a.happyday_dt,5,2),"/",substring(a.happyday_dt,7,2))as HDstartdtShow,  substring(a.happyday_dt,1,8) as HDstartdtDB, concat(substring(a.happyday_dt,9,2),"시",substring(a.happyday_dt,11,2),"분") as HDstarttimeShow, substring(a.happyday_dt,9,4) as HDstarttimeDB,  concat(left(a.dday_dt,4),"/",substring(a.dday_dt,5,2),"/",substring(a.dday_dt,7,2))as HDddaydtShow,  a.dday_dt as HDddaydtDB, concat_ws(",", a.category_code, case a.category_code2 when "" then NULL else a.category_code2 end, case a.category_code3 when "" then NULL else a.category_code3 end) as totalCategory, count(c.user_id) cur_users from happyday_master a, user b, happyday_user_hst c where a.reg_user_id = b.id and a.happyday_id = c.happyday_id and c.state="y" and a.happyday_id = ?;', [req.params.id], function(error, rows) {
             
-            connection.query('select *, concat(left(a.happyday_dt,4),"/",substring(a.happyday_dt,5,2),"/",substring(a.happyday_dt,7,2))as HDstartdtShow,  substring(a.happyday_dt,1,8) as HDstartdtDB, concat(substring(a.happyday_dt,9,2),"시",substring(a.happyday_dt,11,2),"분") as HDstarttimeShow, substring(a.happyday_dt,9,4) as HDstarttimeDB,  concat(left(a.dday_dt,4),"/",substring(a.dday_dt,5,2),"/",substring(a.dday_dt,7,2))as HDddaydtShow,  a.dday_dt as HDddaydtDB from happyday_master a, user b where a.reg_user_id = b.id and a.happyday_id = ?;', req.params.id, function(error, rows) {    
-            console.log("bb");
+            // connection.query('select *, concat(left(a.happyday_dt,4),"/",substring(a.happyday_dt,5,2),"/",substring(a.happyday_dt,7,2))as HDstartdtShow,  substring(a.happyday_dt,1,8) as HDstartdtDB, concat(substring(a.happyday_dt,9,2),"시",substring(a.happyday_dt,11,2),"분") as HDstarttimeShow, substring(a.happyday_dt,9,4) as HDstarttimeDB,  concat(left(a.dday_dt,4),"/",substring(a.dday_dt,5,2),"/",substring(a.dday_dt,7,2))as HDddaydtShow,  a.dday_dt as HDddaydtDB from happyday_master a, user b where a.reg_user_id = b.id and a.happyday_id = ?;', req.params.id, function(error, rows) {    
+            // console.log("bb");
                 if(error) {
                     connection.release();
                     throw error;
@@ -38,13 +36,9 @@ module.exports = function(app, connectionPool) {
     app.post('/happyday_update', function(req, res, next) {
         
         connectionPool.getConnection(function(err, connection) {
-        
-            connection.query('update happyday_master set happyday_name = ?, happyday_contents = ?, category_code=?, dday_dt = ?, happyday_dt = ?, req_point =?, num_participants =?, place_name = ?, img_url =?, point_rsn = ?, update_dtm = date_format(sysdate(), "%Y%m%d%H%i%s") where happyday_id = ?;'                       
-<<<<<<< HEAD
-            , [req.body.happyday_name, req.body.happyday_contents, req.body.category_code, req.body.dday_goto_DB, req.body.startdate_goto_DB, req.body.req_point, req.body.num_participants, req.body.place_name,  req.body.img_url, req.body.point_rsn, req.body.happyID], function(error, rows) {
-=======
-            , [req.body.happyday_name, req.body.happyday_contents, req.body.category_code, req.body.dday_goto_DB, req.body.starthappydaydatetime, req.body.req_point, req.body.num_participants, req.body.place_name,  req.body.img_url, req.body.point_rsn, req.body.happyID], function(error, rows) {
->>>>>>> 0a1b75761ea70661e9fb9fcf4817f00fccd3f827
+        console.log(req.body.category_code3);
+            connection.query('update happyday_master set happyday_name = ?, happyday_contents = ?, category_code=?, dday_dt = ?, happyday_dt = ?, req_point =?, num_participants =?, place_name = ?, img_url =?, point_rsn = ?, update_dtm = date_format(sysdate(), "%Y%m%d%H%i%s"), category_code2 = ?, category_code3 = ? where happyday_id = ?;'                       
+            , [req.body.happyday_name, req.body.happyday_contents, req.body.category_code, req.body.dday_goto_DB, req.body.starthappydaydatetime, req.body.req_point, req.body.num_participants, req.body.place_name,  req.body.img_url, req.body.point_rsn, req.body.category_code2, req.body.category_code3, req.body.happyID], function(error, rows) {
                 if(error) {
                     connection.release();
                     throw error;
