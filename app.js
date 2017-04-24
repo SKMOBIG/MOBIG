3c299cc82fdb232efcc744ed354099e727dc7654
KyeYeon Cho <jgy2157@nate.com>
2017-04-24 00:12:09 +0000

    aksdha

--- app.js
+++ app.js
@@ -73,33 +73,7 @@ connectionPool = mysql.createPool({
     port : '3306',
     connectionLimit : 20,
     waitForConnections : false
-
 }); */
-
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-/*
-var helper = require('sendgrid').mail;
-  
-from_email = new helper.Email("test@example.com");
-to_email = new helper.Email("test@example.com");
-subject = "Sending with SendGrid is Fun";
-content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js");
-mail = new helper.Mail(from_email, subject, to_email, content);
-
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON()
-});
-
-sg.API(request, function(error, response) {
-  console.log(response.statusCode);
-  console.log(response.body);
-  console.log(response.headers);
-})
-*/
 
 //app.get('/', routes.index);
 var index = require('./routes/index')(app, connectionPool);
@@ -113,7 +87,6 @@ var postlist = require('./routes/happyday/postlist')(app, connectionPool);
 var mappopup = require('./routes/happyday/mappopup')(app, connectionPool);
 var hdregpopup = require('./routes/happyday/hdregpopup')(app, connectionPool);
 var hduppopup = require('./routes/happyday/hduppopup')(app, connectionPool);
-var error = require('./routes/happyday/error')(app, connectionPool);
 
 var server = http.createServer(app)
 
