0aded751e8173396766e84899ad8305935ba98da
yonghyun <cyh602@sk.com>
2017-08-09 07:11:42 +0000

    rmv iframe - yonghyun

--- routes/mvno/mvnodatalist.js
+++ routes/mvno/mvnodatalist.js
@@ -7,20 +7,36 @@ module.exports = function(app, connectionPool) {
             res.redirect('/');
         }
 
-        var req_id = req.params.id;
-        console.log('/mvno/mvnodatalist/', req_id);
-        
-        req_id = 8; // test
-        
+        var user_id = req.session.user_id;
+        console.log('/mvno/mvnodatalist/', user_id);
+
         connectionPool.getConnection(function(err, connection) {
-            connection.query('select * from mvno_req_data where user_id=?;', req_id, function(error, rows) {
+            connection.query('SELECT a.*, b.user_name, (SELECT category_grp_dtl_nm FROM mvno_category_grp_lst WHERE category_grp_dtl_id=a.category_grp_dtl_id1) category_grp_dtl_nm1, \
+            (SELECT category_grp_dtl_nm FROM mvno_category_grp_lst WHERE category_grp_dtl_id=a.category_grp_dtl_id2) category_grp_dtl_nm2, \
+            (SELECT category_grp_dtl_nm FROM mvno_category_grp_lst WHERE category_grp_dtl_id=a.category_grp_dtl_id3) category_grp_dtl_nm3  \
+            FROM mvno_req_data a, user b WHERE a.user_id= 8 AND a.user_id = b.id;', user_id, function(error, rows) {
                 if (error) {
                     connection.release();
                     throw error;
                 }
                 else {
                     if (rows.length >= 0) {
-                        res.render('mvno/mvnodatalist', { req_list: rows, req_id: req_id, session: req.session });
+                        var grpId = new Array(rows.length);
+                        var tmp; var year; var month; var day; var hour; var min; var sec;
+
+                        for(var i=0; i<rows.length; i++) {
+                            if (rows[i].category_grp_dtl_nm1 != null){grpId[i] = rows[i].category_grp_dtl_nm1;}
+                            if (rows[i].category_grp_dtl_nm2 != null){grpId[i] = grpId[i] + ", " + rows[i].category_grp_dtl_nm2;}
+                            if (rows[i].category_grp_dtl_nm3 != null){grpId[i] = grpId[i] + ", " + rows[i].category_grp_dtl_nm3;}
+                            year = rows[i].req_dtm.substring(0,4);
+                            month = rows[i].req_dtm.substring(4,6);
+                            day = rows[i].req_dtm.substring(6,8);
+                            hour = rows[i].req_dtm.substring(8,10);
+                            min = rows[i].req_dtm.substring(10,12);
+                            sec = rows[i].req_dtm.substring(12,14);
+                            rows[i].req_dtm = year + ". " + month + ". " + day + ". " + hour + ":" + min + ":" + sec;
+                        }
+                        res.render('mvno/mvnodatalist', { req_list: rows, session: req.session, grpId: grpId });
                         connection.release();
                     }
                     else {
@@ -29,6 +45,7 @@ module.exports = function(app, connectionPool) {
                     }
                 }
             });
+    
         });
     });
     
